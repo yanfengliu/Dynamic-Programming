489bcc56d5d5906b42d34ff07d8a3f406a730aa3
Yanfeng Liu <yanfengliux@gmail.com>
2017-11-15 14:57:39 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,6 +3,7 @@
 fibonacci_list = [0, 1]
 # specify the n th fibonacci number we are interested in
 n = 100
-for i in range(2, 100):
-    fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
+if n > 2:
+    for i in range(2, n):
+        fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
 print('The %d th fibonacci number is %d\n' % (n, fibonacci_list[n-1]))
